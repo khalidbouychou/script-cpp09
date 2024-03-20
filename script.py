f08f03aa52b07999b917a2fe505173b0659671da
Khalid Bouychou <khbouych@e3r4p16.1337.ma>
2024-03-20 20:00:29 +0000

    fixed

--- script.py
+++ script.py
@@ -1,23 +1,35 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    script.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: khbouych <khbouych@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/03/20 19:43:15 by khbouych          #+#    #+#              #
+#    Updated: 2024/03/20 19:45:27 by khbouych         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 import os
 
 import random
 
 for n in range(2, 3000):
-    for test in range(10):
-        args = list(range(n))
-        if (test == 0):
-            args = args[::-1]
+    for data in range(10):
+        input = list(range(n))
+        if (data == 0):
+            input = input[::-1]
         else:
-            random.shuffle(args)
-        oargs = args
-        args = [str(x) for x in args]
-        args = " ".join(args)
-        cmd = "../PmergeMe " + args + " > out.txt"
-        os.system(cmd)
-        after = open("out.txt").read().split("After   :")[1].split("\n")[0]
+            random.shuffle(input)
+        out_args = input
+        input = [str(x) for x in input]
+        input = " ".join(input)
+        command = "../PmergeMe " + input + " > res.txt"
+        os.system(command)
+        after = open("res.txt").read().split("After   :")[1].split("\n")[0]
         after = after.split(" ")
         after = [int(x) for x in after if x.strip()]
-        if (after != sorted(oargs)):
-            print (cmd)
+        if (after != sorted(out_args)):
+            print (command)
             exit()
-        print ("OK", "n: ", n, "test: ", test)
+        print ("OK", "n: ", n, "data: ", data)
